f73457b4e59579a1da13aed501c37972aed24168
Ahmed Yamany <ahmedyamany@localhost.localdomain>
2020-02-05 23:31:44 +0200

    responsive

---
+++ scripts.js
@@ -0,0 +1,10 @@
+//start
+const selectElement = (s) => document.querySelector(s);
+// open
+selectElement('.open').addEventListener('click', () => {
+    selectElement('.nav-list').classList.add('active');
+});
+
+selectElement('.close').addEventListener('click', () => {
+    selectElement('.nav-list').classList.remove('active');
+});
